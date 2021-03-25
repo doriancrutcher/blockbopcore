7e21875ce75c0ed6ab11f7776b6c7bd16c2fcb5d
Dorian Crutcher <doriankinoocrutcher@Dorians-MacBook-Pro.local>
2021-03-25 16:35:08 -0700

    updated rust config

--- contract/target/wasm32-unknown-unknown/release/libgreeter.d
+++ contract/target/wasm32-unknown-unknown/release/libgreeter.d
@@ -1,1 +1,1 @@
-/Users/doriankinoocrutcher/Documents/blockheads/blockbop/blockbop/contract/target/wasm32-unknown-unknown/release/libgreeter.rlib: /Users/doriankinoocrutcher/Documents/blockheads/blockbop/blockbop/contract/src/lib.rs
+/Users/doriankinoocrutcher/Documents/blockheads/blockbopcore/contract/target/wasm32-unknown-unknown/release/libgreeter.rlib: /Users/doriankinoocrutcher/Documents/blockheads/blockbopcore/contract/src/lib.rs
